8d714a083112fc1a5fd99f05dc4057afe7a45476
ddingjin <106426770+uengae@users.noreply.github.com>
2023-07-06 15:25:14 +0900

    filter 사용으로 모든 페이지 utf-8로 인고딩

---
+++ src/main/java/cash/filter/EncodingFilter.java
@@ -0,0 +1,24 @@
+package cash.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+@WebFilter("/*")
+public class EncodingFilter extends HttpFilter implements Filter {
+       
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("서블릿 불러오기 전");
+		
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+		
+		System.out.println("서블릿 불러온 후");
+	}
+
+}
